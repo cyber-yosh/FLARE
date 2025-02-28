ba739fa62b06b8e25a56d53e911ba3261059f00d
Joshua Luo <74257863+cyber-yosh@users.noreply.github.com>
2025-02-27 23:27:07 -0500

    Delete PRISM directory

--- PRISM/sidepanel.js
+++
@@ -1,9 +0,0 @@
-// Content Script
-chrome.runtime.sendMessage({ action: "getStorageValue" }, async function(response) {
-    if (response && response.summarizedText) {
-      document.getElementById("summarizedText").innerText = response.summarizedText;
-      document.getElementById("summarizedText").style.lineHeight = "1.5";
-    }
-  });
-  
-  
